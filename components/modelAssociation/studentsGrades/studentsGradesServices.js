1e19be9f992fed1a0258d6332766c4be9a43107b
FoveIT <tranvantu7597@gmail.com>
2022-01-13 21:16:01 +0700

    config db

--- components/modelAssociation/studentsGrades/studentsGradesServices.js
+++ components/modelAssociation/studentsGrades/studentsGradesServices.js
@@ -169,3 +169,100 @@ module.exports.MakeAsFinalDecision = async (data) => {
     return error;
   }
 };
+
+module.exports.findStudentByStudentGrades = async (a1, a2) => {
+  try {
+    let result;
+    if (a2 == null || a2 == undefined)
+      result = await StudentsClasses.findAll({
+        include: {
+          model: ClassesGradeStructure,
+          where: {
+            "$classesGradeStructures.studentsGrades.id$": a1,
+          },
+        },
+        raw: true,
+        nest: true,
+      });
+    else
+      result = await StudentsClasses.findAll({
+        include: {
+          model: ClassesGradeStructure,
+          where: {
+            "$classesGradeStructures.studentsGrades.studentsClasses_id$": a1,
+            "$classesGradeStructures.studentsGrades.gradeStructure_id$": a2,
+          },
+        },
+        raw: true,
+        nest: true,
+      });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports.configDB = async () => {
+  try {
+    //get all student classes
+    let structures = await ClassesGradeStructure.findAll({
+      include: {
+        model: StudentsClasses,
+        required: true,
+      },
+      nest: true,
+      raw: true,
+    });
+    //then update them
+    for (
+      let structureIndex = 0;
+      structureIndex < structures.length;
+      structureIndex++
+    ) {
+      if (structures[structureIndex].studentsClasses.id != null) {
+        //console.log(structures[structureIndex].studentsClasses.id);
+        const exist = await StudentsClasses.findOne({
+          where: {
+            student_id: structures[structureIndex].studentsClasses.student_id,
+            ClassId: structures[structureIndex].ClassId,
+          },
+          nest: true,
+          raw: true,
+        });
+        if (
+          exist.ClassId != structures[structureIndex].studentsClasses.ClassId
+        ) {
+          await StudentsClasses.destroy({
+            where: {
+              id: structures[structureIndex].studentsClasses.id,
+            },
+          });
+        } else {
+          //console.log({ exist });
+          await StudentsClasses.update(
+            { ClassId: structures[structureIndex].ClassId },
+            {
+              where: {
+                id: structures[structureIndex].studentsClasses.id,
+              },
+            }
+          );
+        }
+      }
+    }
+    //Get them after updating
+    structures = await ClassesGradeStructure.findAll({
+      include: {
+        model: StudentsClasses,
+        required: true,
+      },
+      nest: true,
+      raw: true,
+    });
+    return structures;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
